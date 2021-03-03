e735c0c9f5841b4f3dca50d13cb1004277323602
kaihualin <kl2cb@mail.missouri.edu>
2021-03-03 13:04:19 -0600

    fixed preset abs

--- frontend/src/presetQueries.js
+++ frontend/src/presetQueries.js
@@ -5,18 +5,21 @@ const queries = new Map([
             parametor: "Trial",
             comparator: "==",
             value: "Straight Line",
+            abs: "no abs",
         },
         {
             id: 1,
             parametor: "Blocks",
             comparator: "==",
             value: "Null",
+            abs: "no abs",
         },
         {
             id: 2,
             parametor: "Fore Strides",
             comparator: ">=",
             value: "20",
+            abs: "no abs",
         }
     ]],
     ["Ipsilateral Pushoff", [
@@ -25,18 +28,21 @@ const queries = new Map([
             parametor: "Trial",
             comparator: "==",
             value: "Straight Line",
+            abs: "no abs",
         },
         {
             id: 1,
             parametor: "Blocks",
             comparator: "==",
             value: "Null",
+            abs: "no abs",
         },
         {
             id: 2,
             parametor: "Fore Strides",
             comparator: ">=",
             value: "10",
+            abs: "no abs",
         }
     ]]
 ]);
