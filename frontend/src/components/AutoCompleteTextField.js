31a15cf4d981761d66e69b96d6442f3bb069e0c8
Royal Sanders <royaljsanders@gmail.com>
2021-03-30 20:25:41 -0500

    merge 2

---
+++ frontend/src/components/AutoCompleteTextField.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import uuid from 'react-uuid'
+
+import './AutoCompleteTextField.css';
+
+function AutoCompleteTextField(props) {
+  const [suggestions, setSuggestions] = useState(props.columns);
+
+    const onTextChange = (e, id) => {
+        let value = e.target.value;
+
+        // sanitize out special character (only allow: dot(.), letters and numbers (\w), and space(\s))
+        value = value.replace(/[^.\w\s]/gi, '');
+
+        let s = props.columns
+        if (value.length > 0) {
+            const regex = new RegExp(`^${value}`, 'i');
+            s = props.columns.sort().filter(v => regex.test(v));
+        }
+        setSuggestions(s)
+        props.valueChange(value, id);
+    }
+
+    const suggestSelected = (value, id) => {
+        props.valueChange(value, id);
+        setSuggestions([])
+    }
+
+    const renderSuggestions = (id) => {
+        if (suggestions.length === 0) {
+            return null;
+        }
+        return (
+            <ul>
+                {suggestions.map(column => (<li key={uuid()} onMouseDown={() => suggestSelected(column, id)}>{column}</li>))}
+            </ul>
+        )
+    }
+
+  return (
+    <div className="AutoCompleteTextField">
+        <input type="text" 
+            value={props.value} 
+            onChange={e => onTextChange(e, props.conditionId)}
+        />
+        {renderSuggestions(props.conditionId)}
+    </div>
+  )
+}
+
+export default AutoCompleteTextField
